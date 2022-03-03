128e31f4751e8a33579a10e15759e9c4404b7e68
Anja Schmidt <anjaschmidt@Anjas-MacBook-Air.local>
2022-03-03 15:32:16 -0700

    made multiple todo lists

--- ToDoApp/js/main.js
+++ ToDoApp/js/main.js
@@ -1,24 +1,57 @@
-// let checklist = [{checklist1:[{title: "clean",completed: false}]}
-let myChecklists = {
-  c1: [
-    {
-      title: "Clean room",
-      completed: false
-    },
-    {
-      title: "Feed dog"
-    }
-  ],
-  c2: []
-};
+let allChecklists;
+if(JSON.parse(localStorage.getItem("localChecklist")) === null){
+  console.log("creating new local storage");
+  allChecklists = {
+    cleaning: [
+      {
+        title: "Clean room",
+        completed: false
+      },
+      {
+        title: "Dishes",
+        completed: false
+      }
+    ],
+    petCare: [
+      {
+        title: "Clean hamster cage",
+        completed: false
+      },
+      {
+        title: "Feed dog",
+        completed: false
+      }
+    ]
+  };
+  window.localStorage.setItem("localChecklist", JSON.stringify(allChecklists));
+}
+
+let currentChecklist;
 let screenMode = "checklist";
+let name;
+allChecklists = JSON.parse(localStorage.getItem("localChecklist"));
+//TEMP DEFAULT
+name = "cleaning";
+currentChecklist = allChecklists[name];
+log();
 
-//Puts checklist on screen
-function printChecklist(){
-  const currentChecklist = myChecklists.c1;
-  //Clears so it can be appended
+//Setup
+window.onload = function(){
+  update();
+  document.body.onkeypress = function(e){
+    if(e.keyCode === 32){
+      // changeChecklist("cleaning");
+    }
+    if(e.keyCode === 13){
+      changeChecklist("petCare");
+    }
+  }
+}
+
+
+
+function print(){
   $("#checklist-box").empty();
-
   if(screenMode === "checklist"){
     $("#checklist-box").append(`
       <div class="">
@@ -53,43 +86,57 @@ function printChecklist(){
     `);
     // document.getElementById("add-button").addEventListener("click", pressedAddButton);
   }
-  //Setup checklist box for checklist screenMode
 
 
-}
-
-if(JSON.parse(localStorage.getItem("myChecklist")) === null){
-  window.localStorage.setItem("myChecklist", JSON.stringify(checklist));
-}
-
-window.onload = function(){
-  checklist = JSON.parse(localStorage.getItem("myChecklist"));
-  printChecklist();
-  // floatAlert("Task Completed");
+  let listNames = Object.keys(allChecklists);
+  // console.log(listNames.length);
+  $("#checklist-list").empty();
+  for(let i=0; i<listNames.length; i++){
+    $("#checklist-list").append(
+      `<button type="button" name="button" onclick="changeChecklist('${listNames[i]}')">${listNames[i]}</button>`
+    )
+  }
+  $("#checklist-list").append(
+    `<div class="">
+      <button type="button" name="button">Add checklist</button>
+    </div>`
+  )
 
 }
 function update(){
-  window.localStorage.setItem("myChecklist", JSON.stringify(checklist));
-  printChecklist();
+  allChecklists[name] = currentChecklist;
+  window.localStorage.setItem("localChecklist", JSON.stringify(allChecklists));
+  log();
+  print();
+}
+
+
+function changeChecklist(input){
+  name = input;
+  currentChecklist = allChecklists[name];
+  update();
+  log();
+}
+
+function log(){
+  console.log(`current checklists is ${name}`)
+  // console.table(currentChecklist);
+}
+function title(code){
+  console.log(`Current title is ${currentChecklist[0].title} at ---${code}`)
 }
 
 function addItem(title){
-  checklist.push({title: title, completed: false});
+  currentChecklist.push({title: title, completed: false});
   update();
 }
 function removeItem(num){
-   checklist.splice(num, 1);
+   currentChecklist.splice(num, 1);
    update();
 }
 function itemCompleted(num){
-  // checklist.splice(num, 1);
-  // $(`#item-num-${num}`).children("span").addClass("underline");
-  // $(`#item-num-${num}`).children("span").css("background-color", "red");
-  // $(`#item-num-${num}`).css("background-color", "red");
-  checklist[num].completed = !checklist[num].completed;
-  // (checklist[num].completed) ? checklist[num].completed = false : "You are not eligible to vote yet";
+  currentChecklist[num].completed = !currentChecklist[num].completed;
   update()
-  // floatAlert();
 }
 function clearCompleted(){
   for(let i = 0; i < checklist.length; i++){
@@ -107,16 +154,12 @@ function floatAlert(message){
 
 function pressedPlusButton(){
   screenMode = "newitem";
-  printChecklist();
+  print();
   $("#item-title").focus();
   // addItem("mytitle");
 }
 function pressedAddButton(){
   addItem($("#item-title").val());
   screenMode = "checklist";
-  printChecklist();
+  print();
 }
-function testFunc(){
-  alert("triggered")
-}
-// object.onload = function(){myScript};
