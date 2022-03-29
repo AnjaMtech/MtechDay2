aaa368651c12fe5e953b9e86d81bc824856ecf0b
Anja Schmidt <anjaschmidt@Anjas-MacBook-Air.local>
2022-03-29 15:57:58 -0600

    tried to creat hasChanged() function but failed

--- RaspberrySunday/js/list-manager.js
+++ RaspberrySunday/js/list-manager.js
@@ -1,30 +1,50 @@
+//-----WIP-----
 // setInterval(loop, 100);
 class AnimalList{
   constructor(){
     this.list = [];
-    this.changeDetector = this.list;
+    this.changeDetector = "blank";
+    this.test = "one";
   }
   add(name, color){
-    this.list[this.list.length] = [name, color]
-    purr(`adding ${name} to the list`);
+    // this.list[this.list.length] = [name, color]
   }
   content(){
     return this.list;
   }
+
   hasChanged(){
+    // purr(`${this.changeDetector} != ${this.list}`)
     if(this.changeDetector != this.list){
-      purr(`true`);
+      purr(`has changed`);
+      this.changeDetector = this.list;
       return true;
     }else{
-      purr('false');
-      return false;
+      purr(`has not changed`)
+      // return false;
     }
   }
+
 
 }
 
 const horses = new AnimalList();
 horses.hasChanged();
-horses.add("daisy", "brown");
-horses.content();
-horses.hasChanged();
+
+setInterval(listManagerLoop, 1000);
+function listManagerLoop(){
+  // purr(horses.hasChanged());
+  if(horses.hasChanged()){
+    // purr();
+  }
+}
+
+function saveToLocal(){
+  // window.localStorage.setItem("localAnimalList", JSON.stringify(animalList));
+}
+if(JSON.parse(localStorage.getItem("localAnimalList")) === null){
+  // saveToLocal();
+}else{
+  // animalList = JSON.parse(localStorage.getItem("localAnimalList"));
+}
+//-----WIP-----
