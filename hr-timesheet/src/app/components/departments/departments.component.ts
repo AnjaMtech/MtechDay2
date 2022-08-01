db4353d9ea321d99550b1ebc1142614351548c70
Evoni Feather <EvoniFeather@gmail.com>
2022-08-01 08:24:03 -0600

    commit

---
+++ hr-timesheet/src/app/components/departments/departments.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { DepartmentsService } from '../../services/departments.service';
+
+@Component({
+  selector: 'app-departments',
+  templateUrl: './departments.component.html',
+  styleUrls: ['./departments.component.scss']
+})
+export class DepartmentsComponent implements OnInit {
+
+  constructor(
+    private departmentsService: DepartmentsService,
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+}
